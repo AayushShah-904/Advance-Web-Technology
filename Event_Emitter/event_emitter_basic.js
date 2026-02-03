e0b4e51f70d93b1aed6cede6d580b6978f7c16dd
AayushShah-904 <aayushshah904@gmail.com>
2026-02-03 20:01:18 +0530

    node and express js

---
+++ Event_Emitter/event_emitter_basic.js
@@ -0,0 +1,53 @@
+const EventEmitter=require("events");
+
+class UserEventTracker extends EventEmitter{
+
+    constructor(){
+        super();
+        this.user_login=0;
+        this.user_logout=0;
+        this.user_purchase=0;
+        this.user_update=0;
+
+    }
+    login(username){
+        console.log("User Log In: ",username);
+        this.user_login++;
+        this.emit("login",username);
+    }
+
+    logout(username){
+        console.log("User Log Out: ",username);
+        this.user_logout++;
+        this.emit("logout",username);
+    }
+    update(details){
+        console.log("Profile is Updated to",details);
+        this.user_update++;
+        this.emit("update",details);
+    }
+    buy(item,username,price){
+        this.user_purchase++;
+        this.emit("buy",item,username,price);
+    }
+
+    summary(){
+        console.log("Number of Login",this.user_login);
+        console.log("Number of Logout",this.user_logout);
+        console.log("Number of Purchase",this.user_purchase);
+        console.log("Number of Update",this.user_update);
+    }
+}
+const user=new UserEventTracker();
+
+user.on("buy",(item,username,price)=>{
+    console.log(`${username} bought ${item} for Rs.${price}`)
+})
+
+
+user.login("Aayush");
+user.buy("Laptop","Aayush",131);
+user.update("SHah")
+user.logout("Aayush");
+
+user.summary();
